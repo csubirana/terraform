239820f5dcad9070ea5bc7088aafa17642517f83
csubirana <100561635+csubirana@users.noreply.github.com>
2023-11-16 16:25:50 +0100

    Update variables.tf

--- TP-terraform/variables.tf
+++ TP-terraform/variables.tf
@@ -1,20 +1,26 @@
 variable "resource_group_name" {
+	default="celian"
 }
 
 variable "location" {
 	description = "location where to deploy"
+	default="francecentral"
 }
 
 variable "azurerm_virtual_network" {
+	default="10.0.0.1"
 }
 
 variable "azurerm_virtual_network_name" {
 	description = "entrer le nom du virtual network"
+	default="vndecelian"
 }
 
 variable "azurerm_subnet" {
+	default="10.0.1.0"
 }
 
 variable "azurerm_subnet_name" {
 	description = "entrer le nom du subnet"
+	default="subnetdecelian"
 }
